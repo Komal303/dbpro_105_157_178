1c29e71cab13bc7ff610d694837c40ae2d4b83e3
Maryam Nawaz157 <maryamnawaz983@gmail.com>
2019-04-18 11:49:55 +0500

    3rd Commit

---
+++ SmartSchoolSystem/SmartSchoolSystem/Models/LogIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartSchoolSystem.Models
+{
+    public class LogIn
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Passward { get; set; }
+
+    }
+}
